b5cb044079b7702d9494342f546be504d07bc68a
Konrad Gräfe <konradgraefe@aol.com>
2018-02-28 16:35:28 +0100

    [cosmetic] Fix white spaces
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- autotag.js
+++ autotag.js
@@ -1,18 +1,18 @@
 for(let index = 0; index < msgHdrs.length; index++) {
 	let message = msgHdrs.queryElementAt(index, Ci.nsIMsgDBHdr);
-    if(message.threadParent == nsMsgKey_None) {
-        continue;
-    }
-    let parent = message.folder.msgDatabase.GetMsgHdrForKey(message.threadParent);
-    let parentTags = parent.getStringProperty('keywords');
-    let currentTags = message.getStringProperty('keywords');
+	if(message.threadParent == nsMsgKey_None) {
+		continue;
+	}
+	let parent = message.folder.msgDatabase.GetMsgHdrForKey(message.threadParent);
+	let parentTags = parent.getStringProperty('keywords');
+	let currentTags = message.getStringProperty('keywords');
 
-    let list = currentTags.split(' ');
+	let list = currentTags.split(' ');
 
-    for(let i = 0; i < list.length; i++) {
+	for(let i = 0; i < list.length; i++) {
 		if(currentTags.indexOf(list[i]) < 0) {
-                         parentTags = parentTags + ' ' +list[i];
-        }
-    }
+			parentTags = parentTags + ' ' +list[i];
+		}
+	}
 	message.setStringProperty('keywords', parentTags);
 }
