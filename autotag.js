0b92440a6d0baacfd83e875d64f1abc136ecb0c0
Konrad Gräfe <konradgraefe@aol.com>
2018-03-19 15:55:05 +0100

    Do not add tags if message is already tagged
    This avoids mistakes when executing the filter on whole folders.
    
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- autotag.js
+++ autotag.js
@@ -3,7 +3,10 @@ var arr = Components.classes['@mozilla.org/array;1'].createInstance(Components.i
 for(let index = 0; index < msgHdrs.length; index++) {
 	let message = msgHdrs.queryElementAt(index, Ci.nsIMsgDBHdr);
 	let parentKey = message.threadParent;
-	let parentTags = "";
+
+	if(message.getStringProperty('keywords')) {
+		continue;
+	}
 
 	while(parentKey != nsMsgKey_None) {
 		let parent = message.folder.msgDatabase.GetMsgHdrForKey(parentKey);
