4487643161af492b72be79eed0101964b7e87bee
Konrad Gräfe <konradgraefe@aol.com>
2020-01-14 11:14:53 +0100

    Ignore internal tags from Thunderbird
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- autotag.js
+++ autotag.js
@@ -1,21 +1,28 @@
 var arr = Components.classes['@mozilla.org/array;1'].createInstance(Components.interfaces.nsIMutableArray);
 
+function getMessageKeywords(msg) {
+	return msg.getStringProperty('keywords').split(" ").filter(
+		keyword => {
+			return !["$mailflagbit0", "nonjunk"].includes(keyword);
+		}
+	);
+}
+
 for(let index = 0; index < msgHdrs.length; index++) {
 	let message = msgHdrs.queryElementAt(index, Ci.nsIMsgDBHdr);
 	let parentKey = message.threadParent;
 
-	if(message.getStringProperty('keywords')) {
+	if(getMessageKeywords(message).length > 0) {
 		continue;
 	}
 
 	while(parentKey != nsMsgKey_None) {
 		let parent = message.folder.msgDatabase.GetMsgHdrForKey(parentKey);
-		let parentTags = parent.getStringProperty('keywords');
-
-		if(parentTags) {
+		let parentTags = getMessageKeywords(parent);
+		if(parentTags.length > 0) {
 			arr.clear();
 			arr.appendElement(message, false);
-			message.folder.addKeywordsToMessages(arr, parentTags);
+			message.folder.addKeywordsToMessages(arr, parentTags.join(" "));
 			break;
 		}
 		parentKey = parent.threadParent;
